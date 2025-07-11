ca59a7e5726b3cf96c392d1519ab0fbd7a74130c
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-07-11 12:22:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const CustomerDetails = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="customer-details">
+      <h2>Customer Details</h2>
+      <p>Name: John Doe</p>
+      <p>Email: 1Hl4d@example.com</p>
     </div>
   );
-}
+};
 
-export default App;
+export  default CustomerDetails;
