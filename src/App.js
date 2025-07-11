f665b01d754a70a60280eb14d7ea0e14d85f12ef
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-07-11 18:28:56 +0530

    chamge props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 
 import './App.css';
 
+const user1 = {
+    Avatar: "https://t4.ftcdn.net/jpg/13/66/62/19/360_F_1366621973_KNVZ3b4VTVkOojnUDVGlSYLeiWnJGSjk.jpg",
+    name: 'samanthi',
+    designation: 'java developer',
+    salary: 250000
+},
+user2 = {
+    Avatar: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_hybrid&w=740',
+    name:'sameera' ,
+    designation: 'fulstack developer',
+    salary: 350000
+},
+user3 = {
+    Avatar: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4866.jpg',
+    name: 'konara',
+    designation: 'PHP developer',
+    salary: 50000
+},
+user4 = {
+    Avatar: 'https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg',
+    name: 'shehani',
+    designation: 'python developer',
+    salary: 250000
+}
 
 const UserManager = ()=>{
 
     return(
     <>
         <div className="wrapper" >
-            <User Avatar = "https://t4.ftcdn.net/jpg/13/66/62/19/360_F_1366621973_KNVZ3b4VTVkOojnUDVGlSYLeiWnJGSjk.jpg" name = 'samanthi' designation= 'java developer' salary= '250000'/>
-            <User Avatar='https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_hybrid&w=740' name = 'sameera' designation= 'fulstack developer' salary= '350000'/>
-            <User Avatar='https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4866.jpg' name = 'konara' designation= 'PHP developer' salary= '50000'/>
-            <User Avatar='https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg' name = 'shehani' designation= 'python developer' salary= '250000'/>
+            <User Avatar = {user1.Avatar} name = {user1.name} designation= {user1.designation} salary= {user1.salary}/>
+            <User Avatar = {user2.Avatar} name = {user2.name} designation= {user2.designation} salary= {user2.salary}/>
+            <User Avatar = {user3.Avatar} name = {user3.name} designation= {user3.designation} salary= {user3.salary}/>
+            <User Avatar = {user4.Avatar} name = {user4.name} designation= {user4.designation} salary= {user4.salary}/>
         </div>
     </>
     );
 }
 const User = (props) =>{
-    console.log(props);
+    const {Avatar, name, designation, salary} = props;
     return(
         <div className="user-outer">
-            <Avatar Avatar = {props.Avatar}/>
-           <UserData name = {props.name} designation= {props.designation} salary= {props.salary}/>
+            <AvatarComponent Avatar = {Avatar}/>
+           <UserData name = {name} designation= {designation} salary= {salary}/>
 
 
         </div>
@@ -29,25 +53,25 @@ const User = (props) =>{
     );
 }
 
-const Avatar = (props) =>{
+const AvatarComponent = ({Avatar}) =>{
     return(
         <div className="avatar-outer">
-            <img src={props.Avatar} alt="Avatar-1" className="avatar-1"/>
+            <img src={Avatar} alt="Avatar-1" className="avatar-1"/>
 
         </div>
     );
 }
-const  UserData = (props) =>{
-    console.log(props);
+const  UserData = ({name, designation, salary}) =>{
+    
     return(
         <div className="user-data">
-            <UserName name = {props.name}/>
-            <Designation designation= {props.designation}/>
-            <Salary salary= {props.salary}/>
+            <UserName name = {name}/>
+            <Designation designation= {designation}/>
+            <Salary salary= {salary}/>
         </div>
     );
 }
-const UserName = (props) =>{
+const UserName = ({name}) =>{
     
 
     const inlineStyle = {
@@ -56,28 +80,28 @@ const UserName = (props) =>{
 
     return(
         <div>
-            <h1 className="user-name" style={inlineStyle}>{props.name}</h1>
+            <h1 className="user-name" style={inlineStyle}>{name}</h1>
 
         </div>
     );
 }
 
 
-const Designation = (props) =>{
+const Designation = ({designation}) =>{
     
     return(
         <div>
-            <h3 className="designation" style={{fontStyle:"italic"}}>{props.designation}</h3>
+            <h3 className="designation" style={{fontStyle:"italic"}}>{designation}</h3>
         </div>
     );
 }
 
-const Salary = (props) =>{
+const Salary = ({salary}) =>{
     
     return(
         
         <div>
-            <h3 className="salary" style={{ color: "#2ecc71" }}>{props.salary}</h3>
+            <h3 className="salary" style={{ color: "#2ecc71" }}>{salary}</h3>
         </div>
     );
 }
