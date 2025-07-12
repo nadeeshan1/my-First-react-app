9e0e186bf7a2480ee22e055c967ab4276aa18fa0
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2025-07-12 15:19:46 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,42 +1,68 @@
 
 import './App.css';
-
-const user1 = {
+const users = [
+    {
     Avatar: "https://t4.ftcdn.net/jpg/13/66/62/19/360_F_1366621973_KNVZ3b4VTVkOojnUDVGlSYLeiWnJGSjk.jpg",
     name: 'samanthi',
     designation: 'java developer',
-    salary: 250000
+    salary: 250000,
+    code: 1
 },
-user2 = {
+{
     Avatar: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4841.jpg?semt=ais_hybrid&w=740',
     name:'sameera' ,
     designation: 'fulstack developer',
-    salary: 350000
+    salary: 350000,
+    code: 2
 },
-user3 = {
+{
     Avatar: 'https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4866.jpg',
     name: 'konara',
     designation: 'PHP developer',
-    salary: 50000
+    salary: 50000,
+    code: 3
 },
-user4 = {
+{
     Avatar: 'https://t4.ftcdn.net/jpg/08/23/95/89/360_F_823958944_1c9covIC7Tl7eyJtWoTiXc0L4vP6f43q.jpg',
     name: 'shehani',
     designation: 'python developer',
-    salary: 250000
+    salary: 60000,
+    code: 4
 }
+]
+
 
 const UserManager = ()=>{
 
     return(
     <>
         <div className="wrapper" >
-            <User Avatar = {user1.Avatar} name = {user1.name} designation= {user1.designation} salary= {user1.salary}/>
-            <User Avatar = {user2.Avatar} name = {user2.name} designation= {user2.designation} salary= {user2.salary}/>
-            <User Avatar = {user3.Avatar} name = {user3.name} designation= {user3.designation} salary= {user3.salary}/>
-            <User Avatar = {user4.Avatar} name = {user4.name} designation= {user4.designation} salary= {user4.salary}/>
+            <SearchForm/>
+            <hr/>
+            {
+                users.map((user,key) => {
+                    console.log(key);
+                        return  <User {...user}  key = {user.code} />;
+                    })
+            }
         </div>
+        
     </>
+    );
+}
+
+const SearchForm = () => {
+
+    return (
+        <div className="search-outer">
+            <form onSubmit={(e) => {e.preventDefault();console.log("ManegeSubmitEvent");}} className="search-form">
+
+                <input type="search" onChange={(e) => {console.log("Search Text: ", e.target.value);}} placeholder="Search User" name = "searchText" className="search-input" />
+
+                <button type="submit" className="search-button" onClick={(e) => {
+                console.log("ManegeClickEvent");}}> Search </button>
+            </form>
+        </div>
     );
 }
 const User = (props) =>{
@@ -64,21 +90,29 @@ const AvatarComponent = ({Avatar}) =>{
     );
 }
 const  UserData = ({name, designation, salary,children}) =>{
-    console.log(children);
+    const printData = (e) => {
+        console.log("User Data: ", name, designation, salary);
+    }
+
     return(
+        <>
         <div className="user-data">
             <UserName name = {name}/>
             <Designation designation= {designation}/>
             <Salary salary= {salary}/>
             <p className="description">{children}</p>
+            <button type="submit" style={{cursor: "pointer"}} className="search-button" onClick={printData}> more... </button>
         </div>
+        
+        </>
     );
 }
 const UserName = ({name}) =>{
     
 
     const inlineStyle = {
-        color: "#e74c3c"
+        color: "#e74c3c",
+        margin: "0",
     }
 
     return(
@@ -94,7 +128,7 @@ const Designation = ({designation}) =>{
     
     return(
         <div>
-            <h3 className="designation" style={{fontStyle:"italic"}}>{designation}</h3>
+            <h3 className="designation" style={{fontStyle:"italic",margin: "0"}}>{designation}</h3>
         </div>
     );
 }
@@ -104,7 +138,7 @@ const Salary = ({salary}) =>{
     return(
         
         <div>
-            <h3 className="salary" style={{ color: "#2ecc71" }}>{salary}</h3>
+            <h3 className="salary" style={{ color: "#2ecc71" ,margin: "0"}}>{salary}</h3>
         </div>
     );
 }
